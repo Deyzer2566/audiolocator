aacc2cbfecfcfaae979c0e4cdb5d8280868f00cb
Deyzer2566 <a.kozodoy@yandex.ru>
2025-06-08 21:02:02 +0300

    не используем гетеродин; перебираем частоты в спектрах и находим совпадающие частоты

--- test_find_phases.py
+++ test_find_phases.py
@@ -14,9 +14,9 @@ from numpy import fft
 # freqs = random.randint(100, 300, random.randint(2,10)).reshape(-1,1)
 freqs = np.array([10.,13.])
 T = 2
-P = int((np.ceil(freqs.max()+1))*2*2)
+P = int((np.ceil(freqs.max()+1))*2)
 phase1 = np.array([1,1])
-phase2 = np.array([-np.pi/6,-np.pi/6])
+phase2 = np.array([-np.pi/6,-np.pi/3])
 # lag = np.round((phase2%(2*np.pi))/(2*np.pi*freqs)*P).astype(np.int32)
 # phase2 = lag.astype(np.float32)*(2*np.pi*freqs)/P
 # if phase2 > np.pi:
@@ -24,12 +24,12 @@ phase2 = np.array([-np.pi/6,-np.pi/6])
 x = np.linspace(0,T,int(T*P), endpoint=False).reshape(-1,1)
 s1 = np.cos(2*np.pi*(freqs.reshape(-1,1)@x.T)+phase1.reshape(-1,1)).sum(axis=0)
 s2 = np.cos(2*np.pi*(freqs.reshape(-1,1)@x.T)+phase2.reshape(-1,1)).sum(axis=0)
+spec1 = fft.rfft(s1, n=P)
+spec2 = fft.rfft(s2, n=P)
 # for k,i in enumerate(lag):
 #     s2[k,:i] = 0
 from matplotlib import pyplot as plt
-smesitel = s1*s2
-spec = fft.rfft(smesitel, n=P)
-graphs = [s1, s2, smesitel]
+graphs = [s1, s2]
 _, ax = plt.subplots(nrows=len(graphs), ncols=3)
 for k, i in enumerate(graphs):
     ax[k,0].plot(i[:P])
@@ -37,16 +37,12 @@ for k, i in enumerate(graphs):
     ax[k,2].plot(np.angle(fft.rfft(i, n=P)))
 
 threshold = 1e-3
-catched_freqs = np.where(np.abs(spec)/P*2 > threshold)
-peaks = spec[catched_freqs]
-matrix = np.concatenate((np.array(catched_freqs).reshape(-1,1), np.angle(peaks).reshape(-1,1)), axis=1)
-if 0 in catched_freqs[0]:
-    matrix[0,1] = np.arccos(np.abs(spec[0])/P*2)
-print(matrix, matrix.shape)
+catched_freqs = np.where(np.logical_and(np.abs(spec1)/P*2 > threshold, np.abs(spec2)/P*2 > threshold))
+delta_phases = np.angle(spec2)[catched_freqs] - np.angle(spec1)[catched_freqs]
 
 print('phase1', np.angle(fft.rfft(s1, n=P))[freqs.astype(np.int32)])
 print('phase2', np.angle(fft.rfft(s2, n=P))[freqs.astype(np.int32)])
-print('calc phase', np.angle(spec)[freqs.astype(np.int32)*2])
-print('phases sum', phase1+phase2)
-print('phases sub', phase1-phase2)
+print('calc phase', delta_phases)
+print('phases sum', phase2+phase1)
+print('phases sub', phase2-phase1)
 # plt.show()
