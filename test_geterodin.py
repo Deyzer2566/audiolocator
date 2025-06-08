24c38e55f2f3969328573284008f51ab11ae7bc8
Deyzer2566 <a.kozodoy@yandex.ru>
2025-06-08 20:51:40 +0300

    обнаружение компонент после смесителя

--- test_geterodin.py
+++ test_geterodin.py
@@ -12,11 +12,11 @@ from numpy import fft
 #1/2(cos(d)+cos(2ax+d)+cos((a-b)x+c))+cos((a+b)x+c)+cos((b-a)x+d)+cos((b+a)x+d)+cos c + cos(2bx+c)
 
 # freqs = random.randint(100, 300, random.randint(2,10)).reshape(-1,1)
-freqs = np.array([10.])
+freqs = np.array([10.,13.])
 T = 2
-P = 60
-phase1 = np.array([1])
-phase2 = np.array([-np.pi/6])
+P = int((np.ceil(freqs.max()+1))*2*2)
+phase1 = np.array([1,1])
+phase2 = np.array([-np.pi/6,-np.pi/6])
 # lag = np.round((phase2%(2*np.pi))/(2*np.pi*freqs)*P).astype(np.int32)
 # phase2 = lag.astype(np.float32)*(2*np.pi*freqs)/P
 # if phase2 > np.pi:
@@ -36,8 +36,17 @@ for k, i in enumerate(graphs):
     ax[k,1].plot(np.abs(fft.rfft(i, n=P))/P*2)
     ax[k,2].plot(np.angle(fft.rfft(i, n=P)))
 
+threshold = 1e-3
+catched_freqs = np.where(np.abs(spec)/P*2 > threshold)
+peaks = spec[catched_freqs]
+matrix = np.concatenate((np.array(catched_freqs).reshape(-1,1), np.angle(peaks).reshape(-1,1)), axis=1)
+if 0 in catched_freqs[0]:
+    matrix[0,1] = np.arccos(np.abs(spec[0])/P*2)
+print(matrix, matrix.shape)
+
 print('phase1', np.angle(fft.rfft(s1, n=P))[freqs.astype(np.int32)])
 print('phase2', np.angle(fft.rfft(s2, n=P))[freqs.astype(np.int32)])
 print('calc phase', np.angle(spec)[freqs.astype(np.int32)*2])
-print('real phase', phase1+phase2)
-plt.show()
+print('phases sum', phase1+phase2)
+print('phases sub', phase1-phase2)
+# plt.show()
