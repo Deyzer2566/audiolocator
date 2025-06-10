17c2e9b3d78e3bbc38cb009e3f617b27b5bf936e
Deyzer2566 <a.kozodoy@yandex.ru>
2025-06-10 23:16:46 +0300

    убран SLIP, добавлен вывод fft

--- main.c
+++ main.c
@@ -2,69 +2,51 @@
 #include <stdbool.h>
 
 #define BLOCKSIZE 800
-uint8_t adc_data[2][BLOCKSIZE];
+#pragma pack(push, 1)
+struct packet_t {
+    uint8_t header;
+    uint8_t cnt;
+    uint8_t adc_data[BLOCKSIZE];
+    uint32_t sum;
+    uint8_t end;
+};
+#pragma pack(pop)
+struct packet_t packet[2];
 uint8_t adc_buff_num = 0;
 uint16_t adc_data_counter=0;
 uint16_t adc_data_send_counter=0;
-#define END_CHAR 0x13
-#define ESC_CHAR 0xdd
-#define ESC_END 0x15
-#define ESC_ESC 0xdb
-bool start_send = false;
-bool escaped = false;
-uint8_t escaped_char;
 uint8_t packet_num = 0;
 uint32_t sum = 0;
 void USART2_IRQHandler() {
     if((USART2->SR & USART_SR_TXE) && (USART2->CR1 & USART_CR1_TXEIE)) {
-        uint8_t byte_to_send;
-        if(start_send) {
-            if(escaped) {
-                escaped = false;
-                byte_to_send = escaped_char;
-            } else {
-                uint8_t byte;
-                if(adc_data_send_counter < BLOCKSIZE) {
-                    byte = adc_data[(adc_buff_num+1)%2][adc_data_send_counter];
-                    sum += byte;
-                } else if(adc_data_send_counter == BLOCKSIZE){
-                    byte = packet_num++;
-                } else {
-                    byte = (sum>>(8*(adc_data_send_counter-BLOCKSIZE-1)))&0xff;
-                }
-                adc_data_send_counter++;
-                if(byte == END_CHAR || byte == ESC_CHAR) {
-                    escaped_char = (byte==END_CHAR)?ESC_END:ESC_ESC;
-                    escaped = true;
-                    byte_to_send = ESC_CHAR;
-                } else {
-                    byte_to_send = byte;
-                }
-            }
-        } else {
-            byte_to_send = END_CHAR;
-            start_send = true;
-        }
-        if(adc_data_send_counter == BLOCKSIZE+5 && !escaped) {
-            adc_data_send_counter = 0;
+        USART2->DR = ((uint8_t*)&packet[(adc_buff_num+1)%2])[adc_data_send_counter];
+        adc_data_send_counter++;
+        if(adc_data_send_counter == sizeof(struct packet_t)) {
             USART2->CR1 &= ~USART_CR1_TXEIE;
-            start_send = false;
-            sum = 0;
+            adc_data_send_counter=0;
         }
-        USART2->DR = byte_to_send;
     }
 }
 
 void ADC1_2_IRQHandler() {
     uint16_t adc = ADC1->DR;
-    adc_data[adc_buff_num][adc_data_counter] = adc>>8;
+    packet[adc_buff_num].adc_data[adc_data_counter] = adc>>8;
     adc_data_counter++;
-    adc_data[adc_buff_num][adc_data_counter] = adc;
+    packet[adc_buff_num].adc_data[adc_data_counter] = adc;
     adc_data_counter++;
+    sum += ((adc>>8)&0xff) + (adc&0xff);
     if(adc_data_counter == BLOCKSIZE) {
-        USART2->CR1 |= USART_CR1_TXEIE;
+        packet[adc_buff_num].header = 0x69;
+        packet[adc_buff_num].end = 0x96;
+        packet[adc_buff_num].cnt = packet_num++;
+        packet[adc_buff_num].sum = sum;
+        if(!(USART2->CR1 & USART_CR1_TXEIE))
+            USART2->CR1 |= USART_CR1_TXEIE;
+        else
+            GPIOC->ODR |= GPIO_ODR_ODR13;
         adc_data_counter = 0;
         adc_buff_num = (adc_buff_num+1)%2;
+        sum = 0;
     }
 }
 
@@ -86,7 +68,7 @@ int main() {
 
     RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
     USART2->CR1 |= USART_CR1_UE | USART_CR1_TE;
-    USART2->BRR = 8000000/115200;
+    USART2->BRR = 8000000/200000;
 
     GPIOA->CRL &= ~(GPIO_CRL_CNF0|GPIO_CRL_MODE0);
 
@@ -103,11 +85,9 @@ int main() {
     ADC1->CR2 |= ADC_CR2_CONT;
     ADC1->CR2 |= ADC_CR2_ADON;
     NVIC_EnableIRQ(ADC1_2_IRQn);
+    NVIC_SetPriority(USART2_IRQn, 1);
     NVIC_EnableIRQ(USART2_IRQn);
     while(1) {
-        GPIOC->BSRR = GPIO_BSRR_BR13;
-        for(int i = 0;i<100000;i++);
-        GPIOC->BSRR = GPIO_BSRR_BS13;
-        for(int i = 0;i<100000;i++);
+        __NOP();
     }
 }
--- test.py
+++ test.py
@@ -1,27 +1,45 @@
 import serial
-a = serial.Serial('COM5', 115200, timeout=0.1)
-i = 0
+a = serial.Serial('COM5', 200000, timeout=400/(1000000/(239.5+12.5)))
 d = []
-out = ""
+from matplotlib import pyplot as plt
+plt.ion()
+plot_data = []
+import time
+t = time.time()
+p = 0
+import numpy as np
+from numpy.fft import rfft
+_, ax = plt.subplots(nrows=1, ncols=2)
 while(True):
     try:
-        d += a.read(900)
-        d = d[d.index(0x13):]
-        data = d[1:d.index(0x13, 1)]
-        d = d[d.index(0x13,1):]
-        data = [data[0]]+[data[i] if data[i-1] != 0xdd else (0x13 if data[i] == 0x15 else '0xdd') for i in range(1,len(data))]
-        data = list(filter(lambda x: x != 0xdd, data))
-        data = [i if i != '0xdd' else 0xdd for i in data]
-        s = int.from_bytes(data[801:], 'little')
-        print(len(data))
-        print(data[800], sum(data[:800]), s, sum(data[:800]) == s)
-        data = data[:800]
+        d += a.read(807)
+        d = d[d.index(0x69):]
+        if len(d) < 1+1+800+4+1:
+            raise ValueError
+        data = d[:1+1+800+4+1]
+        d = d[1:]
+        if data[-1] != 0x96:
+            raise ValueError
+        s = int.from_bytes(data[1+1+800:1+1+800+4], 'little')
+        if s != sum(data[2:2+800]):
+            raise ValueError
+        if((p+1)%256 != data[1]):
+            print('lel',p,data[1])
+        p = data[1]
+        data = data[2:800+2]
         data = [int.from_bytes(data[i:i+2], byteorder='big') for i in range(0,len(data),2)]
-        print(max(data))
-        out += '\n'.join(map(str, data))+'\n'
+        plot_data += data
+        if len(plot_data) > 400*3:
+            c = time.time()
+            # print(len(plot_data)/(c-t))
+            t = c
+            plt.pause(0.01)
+            for i in ax:
+                i.cla()
+            plot_data = np.array(plot_data).astype(np.float32)/4096*3.3
+            ax[0].plot(plot_data)
+            ax[1].plot(np.abs(rfft(plot_data)[1:])/len(plot_data)*2)
+            plt.draw()
+            plot_data = []
     except ValueError:
         continue
-    except KeyboardInterrupt:
-        with open('out.csv', 'w') as f:
-            f.write(out)
-        break
