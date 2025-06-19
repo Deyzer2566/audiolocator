06e902b498349a863a0c14868e8bf24f551bf54f
Deyzer2566 <a.kozodoy@yandex.ru>
2025-06-19 22:50:19 +0300

    частота опроса ацп установлена по аппаратному таймеру stm32

--- main.c
+++ main.c
@@ -17,6 +17,7 @@ uint16_t adc_data_counter=0;
 uint16_t adc_data_send_counter=0;
 uint8_t packet_num = 0;
 uint32_t sum = 0;
+extern uint32_t SystemCoreClock;
 void USART2_IRQHandler() {
     if((USART2->SR & USART_SR_TXE) && (USART2->CR1 & USART_CR1_TXEIE)) {
         USART2->DR = ((uint8_t*)&packet[(adc_buff_num+1)%2])[adc_data_send_counter];
@@ -50,43 +51,76 @@ void ADC1_2_IRQHandler() {
     }
 }
 
-__attribute__((noreturn))
-int main() {
+void switch_to_hse() {
     RCC->CR |= RCC_CR_HSEON;
     while(!(RCC->CR & RCC_CR_HSERDY)){}
     RCC->CFGR |= RCC_CFGR_SW_0;
     while(!(RCC->CFGR & RCC_CFGR_SWS_0)){}
+}
 
+void enable_pc13() {
     RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
     GPIOC->CRH &= ~GPIO_CRH_MODE13;
     GPIOC->CRH |= GPIO_CRH_MODE13_0;
     GPIOC->CRH &= ~GPIO_CRH_CNF13;
+}
 
+void init_uart() {
     RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
     GPIOA->CRL &= ~(GPIO_CRL_CNF2|GPIO_CRL_MODE2);
     GPIOA->CRL |= GPIO_CRL_CNF2_1 | GPIO_CRL_MODE2_0;
 
     RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
     USART2->CR1 |= USART_CR1_UE | USART_CR1_TE;
-    USART2->BRR = 8000000/200000;
+    USART2->BRR = SystemCoreClock/200000;
+}
 
+/* 
+ * Таймер опроса АЦП
+ * Частота дискретизации = 8.000.000 Гц/100/10 == 8 кГц
+ */
+void init_tim3() {
+    RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
+    TIM3->PSC = 100-1;
+    TIM3->ARR = 10-1;
+    TIM3->CR2 = /*update event as timer's trigger out*/TIM_CR2_MMS_1;
+    TIM3->EGR = TIM_EGR_TG | TIM_EGR_UG;
+}
+
+void start_adc() {
+    TIM3->CR1 = TIM_CR1_CEN;
+}
+
+void init_adc() {
     GPIOA->CRL &= ~(GPIO_CRL_CNF0|GPIO_CRL_MODE0);
-
     RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
-    RCC->CFGR |= RCC_CFGR_ADCPRE;
-    ADC1->CR2 |= ADC_CR2_ADON;
-    for(int i = 0;i<10;i++) {}
-    ADC1->CR2 |= ADC_CR2_CAL;
-    while(ADC1->CR2 & ADC_CR2_CAL){}
+    ADC1->CR2 |= ADC_CR2_EXTSEL_2;// TIM3 TRGO as external trigger
+    ADC1->CR2 |= ADC_CR2_EXTTRIG;// set external trigger for conversion start
     ADC1->SMPR2 |= ADC_SMPR2_SMP0;
     ADC1->SQR1 &= ~ADC_SQR1_L;
     ADC1->SQR3 &= ~ADC_SQR3_SQ1;
     ADC1->CR1 |= ADC_CR1_EOCIE;
-    ADC1->CR2 |= ADC_CR2_CONT;
     ADC1->CR2 |= ADC_CR2_ADON;
+    ADC1->CR2 |= ADC_CR2_ADON;
+}
+
+__attribute__((noreturn))
+int main() {
+    SystemCoreClock = (uint32_t)8000000;
+    switch_to_hse();
+
+    enable_pc13();
+
+    init_uart();
+
+    init_tim3();
+
+    init_adc();
     NVIC_EnableIRQ(ADC1_2_IRQn);
     NVIC_SetPriority(USART2_IRQn, 1);
     NVIC_EnableIRQ(USART2_IRQn);
+    
+    start_adc();
     while(1) {
         __NOP();
     }
--- test.py
+++ test.py
@@ -1,20 +1,18 @@
 import serial
-a = serial.Serial('COM5', 200000, timeout=400/(1000000/(239.5+12.5)))
+Fs = 8000
+a = serial.Serial('COM5', 200000, timeout=400/Fs)
 d = []
-from matplotlib import pyplot as plt
-plt.ion()
-plot_data = []
 import time
 t = time.time()
 p = 0
 import numpy as np
 from numpy.fft import rfft
-_, ax = plt.subplots(nrows=1, ncols=2)
 while(True):
     try:
         d += a.read(807)
         d = d[d.index(0x69):]
         if len(d) < 1+1+800+4+1:
+            print('wow')
             raise ValueError
         data = d[:1+1+800+4+1]
         d = d[1:]
@@ -23,23 +21,16 @@ while(True):
         s = int.from_bytes(data[1+1+800:1+1+800+4], 'little')
         if s != sum(data[2:2+800]):
             raise ValueError
+        d = d[807-1:]
         if((p+1)%256 != data[1]):
             print('lel',p,data[1])
         p = data[1]
-        data = data[2:800+2]
-        data = [int.from_bytes(data[i:i+2], byteorder='big') for i in range(0,len(data),2)]
-        plot_data += data
-        if len(plot_data) > 400*3:
-            c = time.time()
-            # print(len(plot_data)/(c-t))
-            t = c
-            plt.pause(0.01)
-            for i in ax:
-                i.cla()
-            plot_data = np.array(plot_data).astype(np.float32)/4096*3.3
-            ax[0].plot(plot_data)
-            ax[1].plot(np.abs(rfft(plot_data)[1:])/len(plot_data)*2)
-            plt.draw()
-            plot_data = []
+        # data = data[2:800+2]
+        # data = [int.from_bytes(data[i:i+2], byteorder='big') for i in range(0,len(data),2)]
+        # print(np.abs(rfft(data)[int(1000/Fs*400)])/400*2)
+        c = time.time()
+        if c-t > 0:
+            print(400/(c-t), c-t)
+        t=c
     except ValueError:
         continue
